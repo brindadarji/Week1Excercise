bdff314d69bfc9ad01a2dd4f2c7b411d4a624ee5
brindadarji <brinda.darji19@gmail.com>
2023-10-30 13:49:14 +1300

    commit

---
+++ Classes/CalculatorBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week1CalculatorExercise.Classes;
+
+namespace Week1CalculatorExercise.Classes
+{
+    public abstract class CalculatorBase
+    {
+        public abstract void Add(int ValueOne, int ValueTwo);
+        public abstract void Substract(int ValueOne, int ValueTwo);
+        public abstract void Multiply(int ValueOne, int ValueTwo);
+
+    }
+}
+
+public class Calculator : CalculatorBase 
+{
+    public override void Add(int ValueOne, int ValueTwo)
+    {
+        var AddedValue = ValueOne + ValueTwo;
+        Console.WriteLine("Added value is: " + AddedValue);
+    }
+
+    public override void Substract(int ValueOne, int ValueTwo)
+    {
+        var SubstractedValue = ValueOne - ValueTwo;
+        Console.WriteLine("Substracted value is: " + SubstractedValue);
+    }
+    public override void Multiply(int ValueOne, int ValueTwo)
+    {
+        var MultipliedValue = ValueOne * ValueTwo;
+        Console.WriteLine("Multiplied value is: " + MultipliedValue);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Calculator MyCaculator = new Calculator();
+var ValueOne = 50;
+var ValueTwo = 30;
+Console.WriteLine("ValueOne is: " + ValueOne + " and " + "ValueTwo is: " + ValueTwo);
+Console.WriteLine("See the results below for calculator functions");
+MyCaculator.Add(ValueOne, ValueTwo);
+MyCaculator.Substract(ValueOne, ValueTwo);
+MyCaculator.Multiply(ValueOne, ValueTwo);
